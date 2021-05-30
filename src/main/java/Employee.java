c7636e8208e9feba412d60428c42bb4e48d60d0a
arsentiy82 <arssentiyy@yandex.ru>
2021-05-30 22:00:05 +0300

    oop_1

---
+++ src/main/java/Employee.java
@@ -0,0 +1,23 @@
+public class Employee {
+
+   String fullName;
+   String position;
+   String email;
+   String phone;
+   float salary;
+   int age;
+
+   public Employee(String fullName, String position, String email, String phone, float salary, int age) {
+      this.fullName = fullName;
+      this.position = position;
+      this.email = email;
+      this.phone = phone;
+      this.salary = salary;
+      this.age = age;
+   }
+
+   public void info() {
+      System.out.printf("fullMame = %s, position = %s, email = %s, phone = %s, salary = %f, age = %d", fullName, position, email, phone, salary, age);
+   }
+
+}
---
+++ src/main/java/NewEmployee.java
@@ -0,0 +1,8 @@
+public class NewEmployee {
+
+    public static void main(String[] args) {
+        Employee p1 = new Employee("Иванов Иван Иваныч", "космонавт", "space@gmail.com", "+7-777-777-77-77", 10000.5F, 37 );
+        p1.info();
+    }
+
+}
