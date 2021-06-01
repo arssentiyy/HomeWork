3432cf82fc5da27b2cb018b958fae5347cdd2faf
arsentiy82 <arssentiyy@yandex.ru>
2021-06-01 10:56:02 +0300

    oop_3_private_and_getter

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -1,12 +1,12 @@
 public class Employee {
 
-   String fullName;
-   String position;
-   String email;
-   String phone;
-   float salary;
-   int age;
-   public static int count;
+    private final String fullName; // final значит поле изменять нельзя и метод setter не доступен, только getter
+    private final String position;
+    private final String email;
+    private final String phone;
+    private final float salary;
+    private final int age;
+    public static int count;
 
    public Employee(String fullName, String position, String email, String phone, float salary, int age) {
       count++;
@@ -18,7 +18,11 @@ public class Employee {
       this.age = age;
    }
 
-   public void info() {
+    public int getAge() { // final значит поле изменять нельзя и метод setter не доступен, только getter
+        return age;
+    }
+
+    public void info() {
       System.out.printf("fullMame = %s, position = %s, email = %s, phone = %s, salary = %f, age = %d", fullName, position, email, phone, salary, age);
    }
 
--- src/main/java/NewEmployee.java
+++ src/main/java/NewEmployee.java
@@ -14,7 +14,7 @@ public class NewEmployee {
                 persArray[3] = new Employee("Пупкин Иван Иваныч", "старший инженер", "space3@gmail.com", "+7-777-777-77-50", 10000.5F, 45 );
                 persArray[4] = new Employee("Шмаков Иван Иваныч", "космонавт", "space@gmail.com", "+7-777-777-77-77", 10000.5F, 37 );
         for (int i = 0; i < persArray.length; i++) {
-            if (persArray[i].age > 40) {
+            if (persArray[i].getAge() > 40) {
                 System.out.println();
                 System.out.println("Этот сотрудник старше 40: ");
                 persArray[i].info();
