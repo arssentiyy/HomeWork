fa63a9aad8592779be9154558fe4b543f6337992
arsentiy82 <arssentiyy@yandex.ru>
2021-06-07 18:31:51 +0300

    OOP_dog_and_cat_f_07062021_1830

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -1,5 +1,14 @@
 public abstract class Animal {
+
+    private final String name;
+    private final int satiety;
+
+    public Animal(String name, int satiety) { // satiety - сытость
+        this.name = name;
+        this.satiety = satiety;
+    }
+
     abstract void run(int dist);
     abstract void swimming(int dist);
-    //ssss
+
 }
